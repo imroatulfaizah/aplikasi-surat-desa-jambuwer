66717e8791cec23bf9cedf2f2d4bb82ec9613069
Imroatul Faiseh <imroatul.faiseh@fiberstar.co.id>
2021-06-20 03:56:08 +0700

    register

--- register/index.php
+++ register/index.php
@@ -14,27 +14,41 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="shortcut icon" href="../assets/img/mini-logo.png">
-	<title>Login Page</title>   
+	<title>Register Page</title>   
 	<link rel="stylesheet" href="../assets/fontawesome-5.10.2/css/all.css">
 	<link rel="stylesheet" href="../assets/bootstrap-4.3.1/dist/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="../assets/loginCSS/login.css">
 </head>
 <body>
 <div class="container">
-	<?php 
+<?php 
 		if(isset($_GET['pesan'])){
-			if($_GET['pesan']=="gagal-menambah"){
-				echo "<div class='alert alert-danger'><center>Username atau Password Anda salah!</center></div>";
+			if($_GET['pesan']=="user-exist"){
+				echo "<div class='alert alert-danger'><center>NIK sudah terdaftar! Silahkan masuk ke menu Login</center></div>";
+			}if($_GET['pesan']=="tidak-terdaftar"){
+				echo "<div class='alert alert-danger'><center>NIK tidak terdaftar sebagai warga Jambuwer! Silahkan cek ulang NIK</center></div>";
 			}
 		}
 	?>
 	<div class="d-flex justify-content-center h-100">
 		<div class="card">
 			<div class="card-header text-center mt-4">
-				<h3>Login Sistem</h3>
+				<h3>Form Register</h3>
 			</div>
 			<div class="card-body">
-				<form method="post" action="aksi-login.php">
+				<form method="post" action="simpan_register.php">
+                <div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-user"></i></span>
+						</div>
+						<input type="text" class="form-control" name="nik" placeholder="nik" required>			
+					</div>
+                    <div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-user"></i></span>
+						</div>
+						<input type="text" class="form-control" name="email" placeholder="email" required>			
+					</div>
 					<div class="input-group form-group">
 						<div class="input-group-prepend">
 							<span class="input-group-text"><i class="fas fa-user"></i></span>
@@ -48,14 +62,11 @@
 						<input type="password" class="form-control" name="password" placeholder="password" required>
 					</div><br>
 					<div class="form-group">
-						<input type="submit" value="Login" class="btn float-right login_btn">
+						<input type="submit" value="Register" class="btn float-right login_btn">
 					</div>
 				</form>
 			</div>
 			<div class="card-footer">
-			<div class="d-flex justify-content-center links">
-					<span style="font-size:8pt">Belum Punya Akun? <a href="../register/" class="text-decoration-none text-blue">Register</a></span>
-				</div>
 				<div class="d-flex justify-content-center links">
 					<span style="font-size:8pt">Copyright &copy; 2021 <a href="../" class="text-decoration-none text-white">E-Mailservice</a></span>
 				</div>
---
+++ register/simpan_register.php
@@ -0,0 +1,49 @@
+<?php
+	include ('../config/koneksi.php');
+
+    // if (isset($_POST['submit'])){
+
+        $nik = $_POST['nik'];
+        $email = $_POST['email'];
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $pass = md5($password);
+        // $nama = "tes";
+        // echo "tes";
+        // die();
+        //$nik = "tes";
+
+
+        $qCekPenduduk = mysqli_query($connect, "SELECT * FROM penduduk WHERE nik='$nik'");
+        $row          = mysqli_num_rows($qCekPenduduk);
+        
+        // var_dump($row);
+        // die();
+        // while($tes = mysqli_fetch_array($qCekPenduduk))
+        //     $nama = $tes['nama'];
+        //     var_dump($nama);
+        
+        // var_dump($nama);
+        if($row > 0){
+            $qCekNIK = mysqli_query($connect, "SELECT * FROM login WHERE nama='$nik'");
+            $jumlah          = mysqli_num_rows($qCekNIK);
+            if($jumlah > 0){
+                header('location:index.php?pesan=user-exist');
+            }else{
+                $qTambahUser = "INSERT INTO login VALUES(NULL, '$nik', '$username', '$email', '$pass', 'penduduk')";
+                // var_dump($qTambahUser);
+                $tambahUser = mysqli_query($connect, $qTambahUser);
+                // var_dump($tambahUser);
+                // die();
+                if($tambahUser){
+                    // var_dump("hai");
+                    // die();
+                    header("location:index.php");
+                }
+            }
+            
+        }else{
+            header('location:index.php?pesan=tidak-terdaftar');
+        }
+    // }
+?>
